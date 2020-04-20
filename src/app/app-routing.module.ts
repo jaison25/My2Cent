cd9e428aed684922f0cd0decd697d64d9c936061
jaison25 <33075690+jaison25@users.noreply.github.com>
2020-04-19 21:01:22 -0500

    front bienvenido, ingresos, gastos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { IngresarComponent } from './ingresar/ingresar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { RegistrarseComponent } from './registrarse/registrarse.component';
+import { InicialComponent} from './inicial/inicial.component';
+import { IngresosComponent} from './ingresos/ingresos.component';
+import { GastosComponent} from './gastos/gastos.component';
 
 
 const routes: Routes = [
   {path: '', component: InicioComponent},
   {path: 'ingresar', component: IngresarComponent},
-  {path: 'registrarse', component: RegistrarseComponent}
+  {path: 'registrarse', component: RegistrarseComponent},
+  {path: 'bienvenido', component:InicialComponent},
+  {path: 'ingresos', component:IngresosComponent},
+  {path: 'gastos', component:GastosComponent}
 ];
 
 @NgModule({
