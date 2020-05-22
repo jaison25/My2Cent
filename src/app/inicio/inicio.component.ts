0e7971b43a7c0474f068179d63a2fa0da87f2389
César Cubillos <cesar@soyyo.co>
2020-05-22 00:07:48 -0500

    Se realiza logout

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -12,4 +12,11 @@ export class InicioComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  logOut(){
+    sessionStorage.removeItem('id');
+    sessionStorage.removeItem('mail');
+    sessionStorage.removeItem('nombre');
+    location.href="/";
+  }
+
 }
